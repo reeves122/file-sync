0cb157be06651f5df059dd5a4c5f449ae10bea31
Justin Reeves <reeves122@gmail.com>
2022-05-04 12:27:14 -0500

    do test commit

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"io/ioutil"
+	"time"
 )
 
 func main() {
@@ -13,22 +16,47 @@ func main() {
 	//})
 	r, err := git.PlainOpen("./")
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	err = r.CreateBranch(&config.Branch{
 		Name: "test",
 	})
-
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("test-file.txt", []byte("test"), 0644)
+	if err != nil {
+		panic(err)
 	}
 
 	w, err := r.Worktree()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = w.Add("test-file.txt")
+
 	status, err := w.Status()
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
 	fmt.Printf("git status: \n%s\n", status)
+
+	commit, err := w.Commit("test commit", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "file-sync",
+			Email: "file-sync@example.com",
+			When:  time.Now(),
+		},
+	})
+
+	obj, err := r.CommitObject(commit)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(obj)
 }
