d3b9a8eb8baaeba1541104a0173dc8bffadefc68
Justin Reeves <reeves122@gmail.com>
2022-05-09 11:47:05 -0500

    update main.go

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"github.com/champ-oss/file-sync/pkg/common"
+	"github.com/champ-oss/file-sync/pkg/git/cli"
 	log "github.com/sirupsen/logrus"
+	"os"
 )
 
 const localRepoDir = "./"
@@ -18,12 +21,40 @@ var files = []string{
 
 func main() {
 	log.SetLevel(log.DebugLevel)
-	//
-	//sourceDir, err := cli.CloneSourceRepo("https://github.com/champ-oss/terraform-module-template")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
+	sourceDir, err := cli.Clone("https://github.com/champ-oss/terraform-module-template")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cli.SetAuthor(localRepoDir, user, email)
+	if err != nil {
+		panic(err)
+	}
+
+	err = cli.Fetch(localRepoDir)
+	if err != nil {
+		panic(err)
+	}
+
+	err = cli.Branch(localRepoDir, branchName)
+	if err != nil {
+		panic(err)
+	}
+
+	err = cli.Checkout(localRepoDir, branchName)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := common.CopySourceFiles(files, sourceDir, localRepoDir); err != nil {
+		log.Fatal(err)
+	}
+
+	if modified := cli.AnyModified(localRepoDir, files); modified == false {
+		log.Info("all files are up to date")
+		os.Exit(0)
+	}
+
 	//if err := checkOutBranch(worktree, branchName); err != nil {
 	//	log.Fatal(err)
 	//}
